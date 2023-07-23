5d0fb6d51db7bfcb365a413904acafef83f68aef
Claudio <claudio.anchesi@outlook.it>
2023-07-23 20:15:34 +0200

    Client a durata temporale.

--- client.go
+++ client.go
@@ -15,30 +15,35 @@ func main() {
 	} else {
 		dest = append(dest, os.Args[1:]...)
 	}	
-
+	
+	timer := time.NewTimer(60 * time.Second)
 	b := 0
-	for b < 5 {
-		i := rand.Intn(len(dest))
-		go func() {
-			conn, err := net.Dial("tcp", dest[i] + ":9093")
-			if err != nil {
-				panic(err)
-			}
-			defer conn.Close()
-		
-			message := 
-			"ServiceType: Kubernetes\n" +
-			"\n" +
-			"prova"
-		
-			_, err = conn.Write([]byte(message))
-			if err != nil {
-				panic(err)
-			}
-		
-			fmt.Printf("Sent: %v\nto %s", message, dest[i])
-		}()
-		b++
-		time.Sleep(200 * time.Millisecond)
+	for b == 0 {
+		select {
+			case <-timer.C:
+				b++
+			default:
+				i := rand.Intn(len(dest))
+				go func() {
+					conn, err := net.Dial("tcp", dest[i] + ":9093")
+					if err != nil {
+						panic(err)
+					}
+					defer conn.Close()
+				
+					message := 
+					"ServiceType: Kubernetes\n" +
+					"\n" +
+					"prova"
+				
+					_, err = conn.Write([]byte(message))
+					if err != nil {
+						panic(err)
+					}
+				
+					fmt.Printf("Sent: %v\nto %s", message, dest[i])
+				}()
+				time.Sleep(75 * time.Millisecond)
+		}
 	}
 }
