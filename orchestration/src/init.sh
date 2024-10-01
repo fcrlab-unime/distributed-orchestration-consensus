e32e9175228a2f8c09349c1add688fb7cdda9e6c
gabryk9 <gabriele.morabito96@gmail.com>
2024-10-01 10:47:32 +0200

    debug: debug prints and commands

--- orchestration/src/init.sh
+++ orchestration/src/init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+echo "Mounting the log filesystem...."
 RES=$(mount.glusterfs localhost:/log /log 2>&1)
+echo $RES
 while [[ $RES != "" ]]; do
     sleep 1
     RES=$(mount.glusterfs localhost:/log /log 2>&1)
+    echo $RES
 done
-
+echo "Log filesystem mounted."
+echo "Starting orchestration module..."
 /home/raft/main
+tail -f /dev/null #TO DEBUG IN CASE OF CRASH
