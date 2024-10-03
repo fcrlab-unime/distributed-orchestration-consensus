024d158d454943cffab6ab5fce12d68f8c0846ed
gabryk9 <48953262+gabryk9@users.noreply.github.com>
2024-10-03 11:18:19 +0200

    merge: old repo

--- gluster/connect.sh
+++ gluster/connect.sh
@@ -3,31 +3,24 @@ IP=""
 MASK=""
 while read IFACE; do
     INFOS=($IFACE)
-    if [[ ${INFOS[0]} == *"$NET_IFACE"* && ${INFOS[1]} == "UP" ]]; then
+    if [[ ${INFOS[0]} == *"$NET_IACE"* && ${INFOS[1]} == "UP" ]]; then
         IFS='/' read IP MASK <<< "${INFOS[2]}"
         break
     fi
 done <<< $IFACE_LIST
-#echo $IFACE_LIST
-#echo $IP
+
 DONE=false
-echo "Scanning for peers..."
 while read line; do
-    echo $line
-    RES=$(timeout 1s gluster peer probe $line 2>&1)
-    echo $RES
+    RES=$(gluster peer probe $line 2>&1)
     if grep -vq "localhost" <<< "$RES"; then
         if $(grep -q "success" <<< "$RES") || $(grep -q "already part of" <<< "$RES") ; then
             DONE=true
             break
         fi
     fi
-done <<< $(fping -aqg -i 10 -r 0 $IP/$MASK)
+done <<<$(fping -aqg -i 1 -r 0 $IP/25)
 
-#echo $DONE
-#sleep 5s
 if [[ $DONE == false ]]; then
-    echo "Shared volume not found. Creating one..."
     gluster volume create log $IP:/data force 
     gluster volume start log
 fi
