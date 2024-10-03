15566019f9fa8d77c9347a5d34e1880e87ce2d25
gabryk9 <48953262+gabryk9@users.noreply.github.com>
2024-10-03 11:21:46 +0200

    merge: completing merge with old commits

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-RES=$(mount.glusterfs localhost:/log /log 2>&1)
-while [[ $RES != "" ]]; do
-    sleep 1
-    RES=$(mount.glusterfs localhost:/log /log 2>&1)
-done
-
-/home/raft/main
--- start.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -it --network raft raft bash
