c4ea78a71a83f1face96019cff0f22a65c9a166b
gabryk9 <48953262+gabryk9@users.noreply.github.com>
2024-10-11 14:28:02 +0200

    fix: reintroduces sleep

--- orchestration/scripts/get_ip.sh
+++ orchestration/scripts/get_ip.sh
@@ -2,7 +2,7 @@
 if [[ $1 = "ping" ]]; then
     while true; do
         fping -aqg -i 10 -r 0 $2/$3 | tee /tmp/ip.fifo > /tmp/newip.txt
-#        sleep 5
+        sleep 5
     done
 elif [[ $1 = "nc" ]]; then
     nc -zvn $2 $3 2>&1 > /dev/null | grep succeeded | cut -d\  -f 3
