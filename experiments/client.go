c9e293c2c892e5091eb403b408198cd9355342a3
gabryk9 <48953262+gabryk9@users.noreply.github.com>
2024-10-25 14:33:13 +0200

    fix: tests

---
+++ experiments/client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	fmt.Println("Client module started.")
+	reqN := 1
+	flag.IntVar(&reqN, "f", 1, "Number of requests to send")
+	flag.Parse()
+
+	i, c := 0, 0
+	timer := time.NewTimer(20 * time.Second)
+	message, _ := os.ReadFile(os.Getenv("HOME") + "./test.yaml")
+	for {
+		select {
+		case <-time.After(time.Duration(1000/reqN-50) * time.Millisecond):
+			i = (i + 1) % len(flag.Args())
+			var conn net.Conn
+			var err error
+			//conn, err := net.Dial("tcp", flag.Arg(i)+":9093")
+			for {
+				conn, err = net.Dial("tcp", flag.Arg(i)+":9093")
+				if err != nil {
+					fmt.Println(err)
+					continue // Keep retrying on error
+				}
+				break // Exit loop if no error
+			}
+			//if err != nil {
+			//	fmt.Println(err)
+			//	return
+			//}
+			defer conn.Close()
+			conn.Write(message)
+			fmt.Printf("Sent to %s\n", flag.Arg(i))
+			c++
+			time.Sleep(50 * time.Millisecond)
+		case <-timer.C:
+			fmt.Printf("\nSent %d messages", c)
+			return
+		}
+	}
+}
--- experiments/test.sh
+++ experiments/test.sh
@@ -8,7 +8,7 @@ HOSTS_FILE="hosts.txt"
 #START THE DISTRIBUTED SYSTEM
 echo "Starting the distributed system..."
 UP_COMMAND="cd /home/pi/distributed-orchestration-consensus && docker compose up -d"
-SLEEP_TIME=15
+SLEEP_TIME=25
 
 while IFS= read -r HOST || [[ -n "$HOST" ]]; do
 
@@ -39,8 +39,10 @@ while IFS= read -r HOST || [[ -n "$HOST" ]]; do
 done < "$HOSTS_FILE"
 SELECTED_ADDRESSES=($(shuf -e "${IP_ADDRESSES[@]}" | head -n "$2"))
 echo "Selected gateways: ${SELECTED_ADDRESSES[@]}"
-go run ../client/client.go -f $1 "${SELECTED_ADDRESSES[@]}"
+go run ./client.go -f $1 "${SELECTED_ADDRESSES[@]}"
 sleep $SLEEP_TIME
+
+echo "\n-------------------------------------------"
 
 ##STOP THE DISTRIBUTED SYSTEM
 echo "Stopping the distributed system..."
@@ -58,6 +60,7 @@ while IFS= read -r HOST || [[ -n "$HOST" ]]; do
     echo "Command executed successfully on $USER_HOST"
   else
     echo "Failed to execute command on $USER_HOST"
+    exit 1
   fi
 
   echo "-------------------------------------------"
@@ -75,33 +78,36 @@ PORT=$(echo "$HOST_TO_SAVE" | cut -d ':' -f 2)
 
 mkdir -p ./results/r$1g$2
 
-SAVE_COMMAND="ssh -o StrictHostKeyChecking=no -p $PORT $USER_HOST 'sudo /home/pi/distributed-orchestration-consensus/experiments/backup_test.sh'"
-{ $SAVE_COMMAND; } < /dev/null
-if [ $? -eq 0 ]; then
-  echo "Results saved successfully on $IP_ADDRESS"
-else
-  echo "Failed to save results on $IP_ADDRESS"
-fi
+#SAVE_COMMAND="ssh -o StrictHostKeyChecking=no -p $PORT $USER_HOST 'sudo sh /home/pi/distributed-orchestration-consensus/experiments/backup_test.sh'"
+#{ $SAVE_COMMAND; } < /dev/null
+#if [ $? -eq 0 ]; then
+#  echo "Results saved successfully on $IP_ADDRESS"
+#else
+#  echo "Failed to save results on $IP_ADDRESS"
+#  exit 1
+#fi
+ssh -o StrictHostKeyChecking=no -p $PORT $USER_HOST 'sudo sh /home/pi/distributed-orchestration-consensus/experiments/backup_test.sh'
 echo "-------------------------------------------"
 
 sleep $SLEEP_TIME
 #DOWNLOAD THE RESULTS
-echo "Downloading the results..."
+echo "Downloading the results from $IP_ADDRESS..."
 scp -o StrictHostKeyChecking=no -P $PORT $USER_HOST:/home/pi/results/*.txt ./results/r$1g$2/.
 scp -o StrictHostKeyChecking=no -P $PORT $USER_HOST:/home/pi/results/*.csv ./results/r$1g$2/.
 scp -r -o StrictHostKeyChecking=no -P $PORT $USER_HOST:/home/pi/results/cpu ./results/r$1g$2/.
-echo "Results downloaded successfully from $IP_ADDRESS"
 echo "-------------------------------------------"
 
 #CLEAN THE BACKUP
-echo "Cleaning the backup..."
-CLEAN_COMMAND="ssh -o StrictHostKeyChecking=no -p $PORT $USER_HOST '/home/pi/distributed-orchestration-consensus/experiments/delete_backup.sh'"
-{ $CLEAN_COMMAND; } < /dev/null
-if [ $? -eq 0 ]; then
-  echo "Backup cleaned successfully on $IP_ADDRESS"
-else
-  echo "Failed to clean backup on $IP_ADDRESS"
-fi
+echo "Cleaning the backup on $IP_ADDRESS..."
+ssh -o StrictHostKeyChecking=no -p $PORT $USER_HOST 'sudo sh /home/pi/distributed-orchestration-consensus/experiments/delete_backup.sh'
+#CLEAN_COMMAND="ssh -o StrictHostKeyChecking=no -p $PORT $USER_HOST 'sudo sh/home/pi/distributed-orchestration-consensus/experiments/delete_backup.sh'"
+#{ $CLEAN_COMMAND; } < /dev/null
+#if [ $? -eq 0 ]; then
+#  echo "Backup cleaned successfully on $IP_ADDRESS"
+#else
+#  echo "Failed to clean backup on $IP_ADDRESS"
+#  exit 1
+#fi
 echo "-------------------------------------------"
 
 #DELETE VOLUMES
@@ -119,6 +125,7 @@ while IFS= read -r HOST || [[ -n "$HOST" ]]; do
     echo "Command executed successfully on $USER_HOST"
   else
     echo "Failed to execute command on $USER_HOST"
+    exit 1
   fi
 
   echo "-------------------------------------------"
