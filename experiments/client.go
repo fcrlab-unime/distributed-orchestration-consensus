6d1c6f4b2b3c3902bad28788552c4de8fcd48d8b
gabryk9 <48953262+gabryk9@users.noreply.github.com>
2024-10-29 17:33:40 +0100

    refactor: termination based on number of requests sent

--- experiments/client.go
+++ experiments/client.go
@@ -16,35 +16,45 @@ func main() {
 	flag.Parse()
 
 	i, c := 0, 0
-	timer := time.NewTimer(20 * time.Second)
+	//timer := time.NewTimer(20 * time.Second)
 	message, _ := os.ReadFile(os.Getenv("HOME") + "/distributed-orchestration-consensus/experiments/test.yaml")
 	for {
-		select {
-		case <-time.After(time.Duration(1000/reqN-50) * time.Millisecond):
-			i = (i + 1) % len(flag.Args())
-			var conn net.Conn
-			var err error
-			//conn, err := net.Dial("tcp", flag.Arg(i)+":9093")
-			for {
-				conn, err = net.Dial("tcp", flag.Arg(i)+":9093")
-				if err != nil {
-					fmt.Println(err)
-					continue // Keep retrying on error
-				}
-				break // Exit loop if no error
+		//select {
+		//case <-time.After(time.Duration(1000/reqN-50) * time.Millisecond):
+		i = (i + 1) % len(flag.Args())
+		var conn net.Conn
+		var err error
+		//conn, err := net.Dial("tcp", flag.Arg(i)+":9093")
+		for {
+			conn, err = net.Dial("tcp", flag.Arg(i)+":9093")
+			if err != nil {
+				fmt.Println(err)
+				continue // Keep retrying on error
 			}
-			//if err != nil {
-			//	fmt.Println(err)
-			//	return
-			//}
-			defer conn.Close()
-			conn.Write(message)
+			break // Exit loop if no error
+		}
+		//if err != nil {
+		//	fmt.Println(err)
+		//	return
+		//}
+		//defer conn.Close()
+		//conn.Write(message)
+		_, err = conn.Write(message)
+		if err != nil {
+			fmt.Println("Error sending message:", err)
+		} else {
 			fmt.Printf("Sent to %s\n", flag.Arg(i))
-			c++
-			time.Sleep(50 * time.Millisecond)
-		case <-timer.C:
-			fmt.Printf("\nSent %d messages", c)
+		}
+
+		conn.Close()
+
+		c++
+		time.Sleep(50 * time.Millisecond)
+		//case <-timer.C:
+		if c >= 100 {
+			fmt.Printf("\nSent %d messages\n", c)
 			return
 		}
+		//}
 	}
 }
