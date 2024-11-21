449a5b3b8ba0eb7f8d998ed3caf867934b1795ca
gabryk9 <48953262+gabryk9@users.noreply.github.com>
2024-11-21 14:32:15 +0100

    refactor: paths

--- experiments/backup_test.sh
+++ experiments/backup_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo cp -r /var/lib/docker/volumes/distributed-orchestration-consensus_gluster/_data/* /home/pi/results
+sudo cp -r /var/lib/docker/volumes/distributed-orchestration-consensus_test/_data/* /home/pi/
 sudo chmod 777 -R /home/pi/results
