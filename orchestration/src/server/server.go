2eb45a8133f3751a83699562b5a9a05a045c4d79
gabryk9 <gabriele.morabito96@gmail.com>
2024-12-01 17:07:47 +0100

    fix: passing request index to election

--- orchestration/src/server/server.go
+++ orchestration/src/server/server.go
@@ -239,8 +239,8 @@ func (s *Server) Submit(index ...int) {
 	}
 	<-s.cm.ElectionChan
 	if os.Getenv("TIME") == "1" {
-		s.Times[index[0]].SetDurationAndWrite(index[0], "ENVE", s.GetConsensusModule().StartTime)
-		s.Times[index[0]].SetStartTime("CP")
+		s.Times[s.commands[0].index].SetDurationAndWrite(index[0], "ENVE", s.GetConsensusModule().StartTime)
+		s.Times[s.commands[0].index].SetStartTime("CP")
 		s.cm.Voting(&s.commands[0], s.commands[0].index)
 	} else {
 		s.cm.Voting(&s.commands[0])
