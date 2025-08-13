79f24e868d18cfc758eb72ce38f97a39c06d1ddf
gabryk9 <gabriele.morabito96@gmail.com>
2025-08-13 13:20:16 +0200

    fix: network management

--- simulation/launch.sh
+++ simulation/launch.sh
@@ -2,13 +2,20 @@
 
 # Numero di repliche
 REPLICAS=${1:-3}
-NETWORK_NAME="cluster_net"
+NETWORK_NAME="gluster_net"
+NETWORK_NAME2="orchestration_net"
 
 docker network rm -f "$NETWORK_NAME" 2>/dev/null || true
 docker network create \
   --driver bridge \
   --subnet 192.168.100.0/25 \
   "$NETWORK_NAME"
+
+docker network rm -f "$NETWORK_NAME2" 2>/dev/null || true
+docker network create \
+  --driver bridge \
+  --subnet 192.168.101.0/25 \
+  "$NETWORK_NAME2"
 
 # Loop per avviare n istanze
 for i in $(seq 1 $REPLICAS); do
